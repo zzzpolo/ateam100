bb594e6b1f5e2a7d72d6ef508841168462769b73
yukunl <47676210+yukunl@users.noreply.github.com>
2019-04-22 13:19:27 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
@@ -31,29 +33,83 @@ public class Main extends Application {
 			root.setTop(topLabel);
 			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-          
-			// StartScene
-			Label welcomeArea = new Label("Welcome to Quiz Generator!");
-			
-			//root.setCenter(welcomeArea);
+
+			// STARTSCENE
+			Label welcome = new Label("Welcome to Quiz Generator!");
+
+			// root.setCenter(welcomeArea);
 			Button buttonAddQuestionButton = new Button("Add Single Question");
 			Button buttonLoadQ = new Button("Load Question");
 			Button buttonSave = new Button("Save");
 			Button startButton = new Button("Start");
-			VBox startBox = new VBox(welcomeArea, buttonAddQuestionButton, buttonLoadQ, buttonSave, startButton);
-			welcomeArea.setTextAlignment(TextAlignment.CENTER);
+			VBox startBox = new VBox(welcome, buttonAddQuestionButton, buttonLoadQ, buttonSave, startButton);
+			startBox.setAlignment(Pos.CENTER);// set Alignment
+			startBox.setSpacing(15);// space between
 			root.setCenter(startBox);
 
-			
-			
-			buttonAddQuestionButton.setOnAction(new EventHandler<ActionEvent>() {
+			// set buttons to the same length
+//			buttonAddQuestionButton.setMinWidth(startBox.getPrefWidth());
+//			buttonLoadQ.setMinWidth(startBox.getPrefWidth());
+//			buttonSave.setMinWidth(startBox.getPrefWidth());
+//			startButton.setMinWidth(startBox.getPrefWidth());
 
+			// GENERATEQUIZ
+			BorderPane newrootBorderPane = new BorderPane();
+			Label generateQuizLabel = new Label("Generate Quiz");
+			Label topicLabel = new Label("Topic (MultipleChoice)");
+			TextField topicArea = new TextField();
+			TextArea topics = new TextArea("TopicA\nTopicB\nTopicC\n");
+			TextField quizNum = new TextField();
+			Label NumQuiz = new Label("Number of Quiz");
+			Button startButton2 = new Button("Start");
+			VBox questionBox = new VBox(15, generateQuizLabel, topicLabel, topicArea, topics, NumQuiz, quizNum,
+					startButton2);
+			questionBox.setAlignment(Pos.CENTER);
+			newrootBorderPane.setTop(welcome);
+			newrootBorderPane.setLeft(questionBox);
+			// questionBox.setSpacing(15);
+			// TextArea blankArea = new TextArea();
+			// HBox generateBox = new HBox(questionBox,blankArea);
+
+			// questionBox.setMargin(questionBox.getChildren().get(2), 5);
+			Scene generateQuiz = new Scene(newrootBorderPane);
+
+			// HBox startQuestion = new HBox();
+
+			// Loadquiz
+			Label loadLabel = new Label("Load Json File");
+			Label readFileLabel = new Label("Json (File Path)");
+			TextField filePathField = new TextField();
+			Button loadButton = new Button("Load");
+			BorderPane loadPane = new BorderPane();
+			loadPane.setTop(welcome);
+			loadPane.setCenter(new VBox(loadLabel, readFileLabel, new HBox(filePathField, loadButton)));
+			Scene loadQuiz = new Scene(loadPane);
+			// action for StartButton1
+			startButton.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
-					primaryStage.setScene(start);
-
+					primaryStage.setScene(generateQuiz);
 				}
 			});
+
+			// action for LoadButton1
+			buttonLoadQ.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					primaryStage.setScene(loadQuiz);
+				}
+			});
+
+			// action for startButton
+			startButton2.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					primaryStage.setScene(generateQuiz);
+				}
+			});
+
+		
 
 			// button that needs to be pressed
 			// Button button = new Button("Press me and change scene");
